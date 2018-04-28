67d5456ac9ab3301bcc64f85e6a087ca81981a40
Mike Hong <writemikehong@gmail.com>
2018-04-27 22:54:14 -0500

    firstCommit

---
+++ app/data/friend.js
@@ -0,0 +1,23 @@
+
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Mike Hong',
+      profilePic: 'http://thechive.com/2017/09/18/dog-tweets/',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
